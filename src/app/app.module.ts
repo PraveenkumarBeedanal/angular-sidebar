854d28b0aae6305aaf8a16c0ff0b06fab62b16ac
PraveenkumarBeedanal <st147769@stud.uni-stuttgart.de>
2019-05-19 14:55:21 +0200

    created working routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {FirstPageComponent} from './firstPage/first-page.component';
+import {SecondPageComponent} from './firstPage/secondPage/second-page.component';
+
 import { HelloComponent } from './hello.component';
+import {SidebarModule} from 'primeng/sidebar';
+
+const appRoutes: Routes = [
+  { path: 'firstPage', component: FirstPageComponent },
+  { path: 'secondPage', component: SecondPageComponent },
+
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    BrowserModule, FormsModule, SidebarModule ],
+  declarations: [ AppComponent, HelloComponent, 
+  FirstPageComponent, SecondPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/firstPage/first-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-first-page',
+  templateUrl: './first-page.component.html',
+  styleUrls: [ './first-page.component.css' ]
+})
+export class FirstPageComponent  {
+
+}
